57c98cc3339adc95b7336573bd1c4f77aa0448be
Pavel Anni <panni@redhat.com>
2019-12-06 14:20:39 -0500

    Add links

--- release_tag.sh
+++ release_tag.sh
@@ -7,15 +7,15 @@
 git tag -l 
 
 # Ask a user for the tag to use
-echo "Set this release tag. Hint: use Semantic Versioning."
-echo "MAJOR: X.y.z, MINOR: x.Y.z, PATCH: x.y.Z"
-echo -n "Tag: "
+printf "Set tag for this release. Hint: use Semantic Versioning.\n"
+printf "MAJOR: X.y.z, MINOR: x.Y.z, PATCH: x.y.Z\n"
+printf -n "Tag: "
 read NEW_TAG
-echo "New tag is: ${NEW_TAG}"
+printf "New tag is: ${NEW_TAG}\n\n"
 
 git checkout master
 # Add and commit all
-echo "Tagging master version ${NEW_TAG}"
+printf "Tagging master version ${NEW_TAG}\n"
 git add --all
 git commit -m "Tag master version ${NEW_TAG}"
 git tag -a ${NEW_TAG} -m "Tag master version ${NEW_TAG}"
@@ -24,12 +24,13 @@ git push origin --tags
 git push -u origin master
 
 # Delete previous Release and create a new one
-git push origin --delete Release
+# git push origin --delete Release
+git branch -D Release
 git checkout -b Release ${NEW_TAG}
 
 # Edit README.adoc
 TODAY_DATE=$(date "+%Y-%m-%d")
-echo "* ${NEW_TAG} - ${TODAY_DATE} - " >> README.adoc
+printf "* ${NEW_TAG} - ${TODAY_DATE} - " >> README.adoc
 vim "+normal GA " +startinsert README.adoc
 
 # Commit and push the Release branch
@@ -41,7 +42,7 @@ git tag -a ${NEW_TAG}R -m "Tag Release version ${NEW_TAG}R"
 git checkout master
 
 # Report and further instructions
-echo "You have successfully prepared your course for the next release."
-echo "Now go to Jenkins and build the course materials with the following parameters:"
-echo "BRANCH_NAME: Release"
-echo "RELEASE_TYPE: COURSE UPDATE (MINOR) or COURSE UPDATE (MAJOR)"
+printf "\nYou have successfully prepared your course for the next release.\n"
+printf "Now go to Jenkins and build the course materials with the following parameters:\n\n"
+printf "BRANCH_NAME: Release\n"
+printf "RELEASE_TYPE: COURSE UPDATE (MINOR) or COURSE UPDATE (MAJOR)\n\n"
